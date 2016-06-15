a12895fc10b079168ff5906c561dc7d48b8f7d9a
Dan Johnson <danj@comms-express.com>
2016-06-15 18:56:58 +0100

    Made some changes to the log in page@

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserify = require('browserify');
+var transform = require('vinyl-transform');
 
 gulp.task('default', function(){
 	gulp.run('scss');
+    gulp.run('js');
     gulp.watch('./app/resources/scss/**/*.scss', ['scss']);
+    gulp.watch('./app/resources/js/**/*.js', ['js']);
 });
 
 gulp.task('scss', function(){
 	gulp.src('./app/resources/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('js', function(){
+    var browserified = transform(function(filename){
+        var b = browserify(filename);
+        return b.bundle();
+    });
+
+    // gulp.src(['./app/resources/js/**/*.js'])
+    //      .pipe(browserified);
+    //      .pipe(gulp.dest('./public/js'));
 });
